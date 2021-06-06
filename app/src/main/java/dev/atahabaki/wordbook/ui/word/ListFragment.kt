a8f2089dc787128358bb3558a0bf059f4aad2f6e
A. Taha Baki <a.taha.baki@outlook.com>
2021-06-06 14:49:57 +0300

    ✨  ListFragment created...

---
+++ app/src/main/java/dev/atahabaki/wordbook/ui/word/ListFragment.kt
@@ -0,0 +1,10 @@
+package dev.atahabaki.wordbook.ui.word
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import dev.atahabaki.wordbook.R
+
+@AndroidEntryPoint
+class ListFragment: Fragment(R.layout.fragment_list_wordbook) {
+    
+}
