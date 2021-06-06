7b4222b57b077c08cf6824092c9119e5e328cdea
A. Taha Baki <a.taha.baki@outlook.com>
2021-06-06 21:50:54 +0300

    ✨ DatabaseModule created...

---
+++ app/src/main/java/dev/atahabaki/wordbook/di/DatabaseModule.kt
@@ -0,0 +1,41 @@
+/*
+ *  WordBook - An android application for those who aims to learn a new language.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of WordBook.
+ *
+ *  WordBook is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WordBook is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WordBook.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.atahabaki.wordbook.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.atahabaki.wordbook.data.AppDatabase
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context) = AppDatabase.getInstance(context)
+
+    @Provides
+    fun provideWordDao(appDatabase: AppDatabase) = appDatabase.wordDao()
+}
