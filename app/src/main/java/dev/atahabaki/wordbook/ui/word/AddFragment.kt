6c65637a95743e3226fd1fde8554bbabe9cfe4d7
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-07 04:02:24 +0300

    ✨  AndroidEntryPoint di added...

--- app/src/main/java/dev/atahabaki/wordbook/ui/word/AddFragment.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/word/AddFragment.kt
@@ -1,6 +1,8 @@
 package dev.atahabaki.wordbook.ui.word
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import dev.atahabaki.wordbook.R
 
+@AndroidEntryPoint
 class AddFragment: Fragment(R.layout.fragment_add)
