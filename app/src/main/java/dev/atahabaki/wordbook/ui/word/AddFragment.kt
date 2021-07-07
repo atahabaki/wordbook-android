01ddb75be26a19b28373f3e34c64052b9c4096ee
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-07 04:18:41 +0300

    ✨  basic binding initialized for AddFragment...

--- app/src/main/java/dev/atahabaki/wordbook/ui/word/AddFragment.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/word/AddFragment.kt
@@ -1,8 +1,31 @@
 package dev.atahabaki.wordbook.ui.word
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import dev.atahabaki.wordbook.R
+import dev.atahabaki.wordbook.databinding.FragmentAddBinding
 
 @AndroidEntryPoint
-class AddFragment: Fragment(R.layout.fragment_add)
+class AddFragment: Fragment(R.layout.fragment_add) {
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
