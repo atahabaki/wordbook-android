f53b9eff966700cf19a13f56263314ba984cfae8
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-10 07:23:40 +0300

    ✨ add searchQuery param to Filter enum class...

--- app/src/main/java/dev/atahabaki/wordbook/utils/Filter.kt
+++ app/src/main/java/dev/atahabaki/wordbook/utils/Filter.kt
@@ -1,7 +1,7 @@
 package dev.atahabaki.wordbook.utils
 
-enum class Filter(val value: Int) {
-    SHOW_ALL(0),
-    SHOW_ONLY_FAV(1),
-    SHOW_ONLY_NOT_FAV(2)
+enum class Filter(val value: Int, val searchQuery: String) {
+    SHOW_ALL(0, ""),
+    SHOW_ONLY_FAV(1, "is:fav"),
+    SHOW_ONLY_NOT_FAV(2, "is:!fav")
 }
