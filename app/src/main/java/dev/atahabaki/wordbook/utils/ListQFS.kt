3648b99ecfc46569078dc1811fa5b7548be09b79
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-10 07:30:00 +0300

    🚚 moving integer extension functions to ExtInt file...

--- app/src/main/java/dev/atahabaki/wordbook/utils/ListQFS.kt
+++ app/src/main/java/dev/atahabaki/wordbook/utils/ListQFS.kt
@@ -20,27 +20,6 @@
 
 package dev.atahabaki.wordbook.utils
 
-fun Int.getSort(): Sort = when (this) {
-    Sort.BY_TITLE_ASC.value -> Sort.BY_TITLE_ASC
-    Sort.BY_TITLE_DESC.value -> Sort.BY_TITLE_DESC
-    Sort.BY_MEAN_ASC.value -> Sort.BY_MEAN_ASC
-    Sort.BY_MEAN_DESC.value -> Sort.BY_MEAN_DESC
-    Sort.BY_DATE_ASC.value -> Sort.BY_DATE_ASC
-    Sort.BY_DATE_DESC.value -> Sort.BY_DATE_DESC
-    Sort.BY_FAV_ASC.value -> Sort.BY_FAV_ASC
-    Sort.BY_FAV_DESC.value -> Sort.BY_FAV_DESC
-    Sort.BY_ID_ASC.value -> Sort.BY_ID_ASC
-    Sort.BY_ID_DESC.value -> Sort.BY_ID_DESC
-    else -> Sort.BY_FAV_DESC
-}
-
-fun Int.getFilter(): Filter = when(this) {
-    Filter.SHOW_ALL.value -> Filter.SHOW_ALL
-    Filter.SHOW_ONLY_FAV.value -> Filter.SHOW_ONLY_FAV
-    Filter.SHOW_ONLY_NOT_FAV.value -> Filter.SHOW_ONLY_NOT_FAV
-    else -> Filter.SHOW_ALL
-}
-
 fun Triple<String, Filter, Sort>.generateQuery() : String {
     var genQuery = """
                 SELECT * FROM wordbook WHERE (LOWER(title) LIKE '%${this.first}%'
