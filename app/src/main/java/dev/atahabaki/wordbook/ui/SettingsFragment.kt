0dfb98777f8a221b922d891ae141e01fbcc5f8d0
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-12 10:16:37 +0300

    ✨  basic fragment set up...

---
+++ app/src/main/java/dev/atahabaki/wordbook/ui/SettingsFragment.kt
@@ -0,0 +1,40 @@
+package dev.atahabaki.wordbook.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import dev.atahabaki.wordbook.R
+import dev.atahabaki.wordbook.databinding.FragmentSettingsBinding
+import dev.atahabaki.wordbook.ui.word.WordViewModel
+
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    private val wordViewModel: WordViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
