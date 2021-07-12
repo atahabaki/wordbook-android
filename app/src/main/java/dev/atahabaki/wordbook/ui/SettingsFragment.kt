b4ecdc7f0e841aa1fff7726754cd6fc1a5133078
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-12 13:57:33 +0300

    📄 added missing copyright notices...

--- app/src/main/java/dev/atahabaki/wordbook/ui/SettingsFragment.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/SettingsFragment.kt
@@ -1,3 +1,23 @@
+/*
+ *  WordBook - An android application for those who aims to learn a new language.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of WordBook.
+ *
+ *  WordBook is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WordBook is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WordBook.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package dev.atahabaki.wordbook.ui
 
 import android.os.Bundle
