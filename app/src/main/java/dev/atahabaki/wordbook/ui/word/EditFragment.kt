a16ecb42d6781ec8e78f9f7db41dc6138bdc9568
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-14 21:43:08 +0300

    ✨  get word entry w/ the navArgs...

--- app/src/main/java/dev/atahabaki/wordbook/ui/word/EditFragment.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/word/EditFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dev.atahabaki.wordbook.R
 import dev.atahabaki.wordbook.databinding.FragmentEditBinding
 
-class EditFragment: Fragment() {
+class EditFragment: Fragment(R.layout.fragment_edit) {
     private var _binding: FragmentEditBinding? = null
     private val binding get() = _binding!!
+
+    private val args: EditFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +23,14 @@ class EditFragment: Fragment() {
     ): View {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            editTitle.setText(args.word.title)
+            editMeaning.setText(args.word.meaning)
+        }
     }
 
     override fun onDestroyView() {
