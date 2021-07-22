21fd3a423cc20b6dad290a31cdb3c222bac97f57
A. Taha Baki <a.taha.baki@outlook.com>
2021-07-22 17:32:53 +0300

    💄 render swipe icons basic impl...

--- app/src/main/java/dev/atahabaki/wordbook/ui/word/ListFragment.kt
+++ app/src/main/java/dev/atahabaki/wordbook/ui/word/ListFragment.kt
@@ -88,92 +88,119 @@ class ListFragment: Fragment(R.layout.fragment_list_wordbook) {
         setupSwipeOperations()
     }
 
-    private fun setupSwipeOperations() {
-        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0,
-            ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
-            fun delete(position: Int) {
-                wordViewModel.onItemDeleted(wAdapter.currentList[position])
-                wAdapter.notifyItemRemoved(position)
-            }
+    private fun setupSwipeOperations() = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0,
+        ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
+        fun delete(position: Int) {
+            wordViewModel.onItemDeleted(wAdapter.currentList[position])
+            wAdapter.notifyItemRemoved(position)
+        }
 
-            fun toggleFavorite(position: Int) {
-                val previous = wAdapter.currentList[position]
-                wordViewModel.insert(previous.copy(isFavorite = !previous.isFavorite))
-                wAdapter.notifyItemChanged(position)
-            }
+        fun toggleFavorite(position: Int) {
+            val previous = wAdapter.currentList[position]
+            wordViewModel.insert(previous.copy(isFavorite = !previous.isFavorite))
+            wAdapter.notifyItemChanged(position)
+        }
 
-            override fun onMove(
-                    recyclerView: RecyclerView,
-                    viewHolder: RecyclerView.ViewHolder,
-                    target: RecyclerView.ViewHolder
-                ): Boolean {
-                return false
-            }
+        override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+            return false
+        }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-                    requireContext().settingsDataStore.data.first().apply {
-                        // From RIGHT to LEFT...
-                        if (direction == ItemTouchHelper.RIGHT) {
-                            when(swipeLeftAction.getSwipeOperation()) {
-                                SwipeOperation.DELETE -> delete(viewHolder.adapterPosition)
-                                SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE ->
-                                    toggleFavorite(viewHolder.adapterPosition)
-                            }
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+                requireContext().settingsDataStore.data.first().apply {
+                    // From RIGHT to LEFT...
+                    if (direction == ItemTouchHelper.RIGHT) {
+                        when(swipeLeftAction.getSwipeOperation()) {
+                            SwipeOperation.DELETE -> delete(viewHolder.adapterPosition)
+                            SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE ->
+                                toggleFavorite(viewHolder.adapterPosition)
                         }
-                        // From LEFT to RIGHT...
-                        else if (direction == ItemTouchHelper.LEFT) {
-                            when(swipeRightAction.getSwipeOperation()) {
-                                SwipeOperation.DELETE -> delete(viewHolder.adapterPosition)
-                                SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE ->
-                                    toggleFavorite(viewHolder.adapterPosition)
-                            }
+                    }
+                    // From LEFT to RIGHT...
+                    else if (direction == ItemTouchHelper.LEFT) {
+                        when(swipeRightAction.getSwipeOperation()) {
+                            SwipeOperation.DELETE -> delete(viewHolder.adapterPosition)
+                            SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE ->
+                                toggleFavorite(viewHolder.adapterPosition)
                         }
                     }
                 }
             }
+        }
 
-            override fun onChildDraw(
-                c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
-                dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
-            ) {
-                viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-                   requireContext().settingsDataStore.data.first().apply {
-                       // Swipe from RIGHT to LEFT...
-                       ColorDrawable().also {
-                           when (swipeLeftAction) {
-                               SwipeOperation.DELETE.value -> it.color = ContextCompat
-                                   .getColor(requireContext(), R.color.trash_background)
-                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value ->
-                                   it.color = ContextCompat.getColor(
-                                       requireContext(), R.color.star_background)
-                           }
-                           it.setBounds(
-                               viewHolder.itemView.left, viewHolder.itemView.top,
-                               viewHolder.itemView.left + dX.toInt(), viewHolder.itemView.bottom
-                           )
-                       }.draw(c)
-                       // Swipe from LEFT to RIGHT...
-                       ColorDrawable().also {
+        override fun onChildDraw(
+            c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+            dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+        ) {
+            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+               requireContext().settingsDataStore.data.first().apply {
+                   if (dX > 0) {
+                       val bg = ColorDrawable(ContextCompat.getColor(
+                           viewHolder.itemView.context,
                            when (swipeRightAction) {
-                               SwipeOperation.DELETE.value -> it.color = ContextCompat
-                                               .getColor(requireContext(), R.color.trash_background)
-                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value ->
-                                       it.color = ContextCompat.getColor(
-                                               requireContext(), R.color.star_background)
+                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value -> R.color.star_background
+                               SwipeOperation.DELETE.value -> R.color.trash_background
+                               else -> R.color.trash_background
                            }
-                           it.setBounds(
-                               viewHolder.itemView.right, viewHolder.itemView.top,
-                               viewHolder.itemView.right + dX.toInt(), viewHolder.itemView.bottom
-                           )
-                       }.draw(c)
+                       ))
+                       bg.setBounds(
+                           viewHolder.itemView.left, viewHolder.itemView.top,
+                           viewHolder.itemView.left + dX.toInt(), viewHolder.itemView.bottom
+                       )
+                       bg.draw(c)
+                       val icon = ContextCompat.getDrawable(viewHolder.itemView.context,
+                           when (swipeRightAction) {
+                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value -> R.drawable.ic_star_filled
+                               SwipeOperation.DELETE.value -> R.drawable.ic_trash_all
+                               else -> R.drawable.ic_trash_all
+                           })!!
+                       val margin = (viewHolder.itemView.height - icon.intrinsicHeight)/2
+                       icon.setBounds(
+                           viewHolder.itemView.left + margin, viewHolder.itemView.top + margin,
+                           viewHolder.itemView.left + margin + icon.intrinsicWidth,
+                           viewHolder.itemView.bottom - margin
+                       )
+                       icon.draw(c)
                    }
-                }
-                super.onChildDraw(
-                    c, recyclerView, viewHolder, dX,
-                    dY, actionState, isCurrentlyActive
-                )
+                   else if (dX < 0) { // LEFT 2 RIGHT...
+                       val bg = ColorDrawable(ContextCompat.getColor(
+                           viewHolder.itemView.context,
+                           when (swipeLeftAction) {
+                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value -> R.color.star_background
+                               SwipeOperation.DELETE.value -> R.color.trash_background
+                               else -> R.color.trash_background
+                           }
+                       ))
+                       bg.setBounds(
+                           viewHolder.itemView.right + dX.toInt(), viewHolder.itemView.top,
+                           viewHolder.itemView.right, viewHolder.itemView.bottom
+                       )
+                       bg.draw(c)
+                       val icon = ContextCompat.getDrawable(viewHolder.itemView.context,
+                           when (swipeLeftAction) {
+                               SwipeOperation.DELETE.value -> R.drawable.ic_trash_all
+                               SwipeOperation.MARK_OR_UNMARK_AS_FAVORITE.value -> R.drawable.ic_star_filled
+                               else -> R.drawable.ic_star_filled
+                           })!!
+                       val margin = (viewHolder.itemView.height - icon.intrinsicHeight)/2
+                       icon.setBounds(
+                           viewHolder.itemView.right - margin - icon.intrinsicWidth,
+                           viewHolder.itemView.top + margin,
+                           viewHolder.itemView.right - margin,
+                           viewHolder.itemView.bottom - margin
+                       )
+                       icon.draw(c)
+                   }
+               }
             }
-        }).attachToRecyclerView(binding.wordsList)
-    }
+            super.onChildDraw(
+                c, recyclerView, viewHolder, dX,
+                dY, actionState, isCurrentlyActive
+            )
+        }
+    }).attachToRecyclerView(binding.wordsList)
 }
