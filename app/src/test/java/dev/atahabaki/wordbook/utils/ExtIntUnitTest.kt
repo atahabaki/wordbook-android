191ed244d96941a7228ed8e00f4ec3a11979a77f
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-07-30 20:23:38 +0300

    ✅  add test cases for getting the right filter type...

--- app/src/test/java/dev/atahabaki/wordbook/utils/ExtIntUnitTest.kt
+++ app/src/test/java/dev/atahabaki/wordbook/utils/ExtIntUnitTest.kt
@@ -14,4 +14,14 @@ class ExtIntUnitTest {
     fun `getSort(not in range) returns BY_FAV_DESC`() {
         assertThat(300.getSort()).isEqualTo(Sort.BY_FAV_DESC)
     }
+
+    @Test
+    fun `getFilter(0) returns SHOW_ALL`() {
+        assertThat(0.getFilter()).isEqualTo(Filter.SHOW_ALL)
+    }
+
+    @Test
+    fun `getFilter(not in range) returns SHOW_ALL`() {
+        assertThat(300.getFilter()).isEqualTo(Filter.SHOW_ALL)
+    }
 }
