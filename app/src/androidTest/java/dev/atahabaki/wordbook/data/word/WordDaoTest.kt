43d7af0ea285ec9c25cd9aa8dc2faa0fd9d0bd99
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-08-14 17:14:10 +0300

    ✨ basic test class created for WordDao...

---
+++ app/src/androidTest/java/dev/atahabaki/wordbook/data/word/WordDaoTest.kt
@@ -0,0 +1,17 @@
+package dev.atahabaki.wordbook.data.word
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import dev.atahabaki.wordbook.data.AppDatabase
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class WordDaoTest {
+    private lateinit var db: AppDatabase
+    private lateinit var dao: WordDao
+}
