3e90149d0860244fb7c54dfc377b3bdb1276e47e
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-09-18 02:07:13 +0300

    ✨ enum cls NotificationPeriod 4 type conversion...

---
+++ app/src/main/java/dev/atahabaki/wordbook/utils/NotificationPeriod.kt
@@ -0,0 +1,33 @@
+/*
+ *  WordBook - An android application for those who aims to learn a new language.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of WordBook.
+ *
+ *  WordBook is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  WordBook is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with WordBook.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.atahabaki.wordbook.utils
+
+import androidx.annotation.StringRes
+import dev.atahabaki.wordbook.R
+
+enum class NotificationPeriod(val value: Int, @StringRes val period: Int) {
+    MIN_15(0, R.string.period_min_15),
+    MIN_30(0, R.string.period_min_30),
+    HOUR_1(0, R.string.period_1_hour),
+    HOURS_2(0, R.string.period_2_hours),
+    HOURS_4(0, R.string.period_4_hours),
+    DAILY(0, R.string.period_daily)
+}
